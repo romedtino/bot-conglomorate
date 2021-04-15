9ea128a14a61f4590ede1862c7ab8207286bbb45
romedtino <romedtino@gmail.com>
2021-04-14 23:21:08 -0700

    Updating 4th for avengers

--- commands/4th.js
+++ commands/4th.js
@@ -1,4 +1,10 @@
 var Datastore = require("nedb");
+const editly = require('editly');
+const path = require("path");
+
+var out_dir = "/usr/share/hassio/homeassistant/www";
+var data_dir = "/home/dark/discord_bots/bot-conglomorate/data";
+var host_url = "https://darkhq.duckdns.org/local/";
 
 const summons = [
   "https://cdn.discordapp.com/attachments/463864401011671050/533917047495589888/PicsArt_01-12-11.41.34.jpg",
@@ -18,7 +24,18 @@ const squad_names = [
 ];
 
 const total = 4;
-const timeout_sec = 10 * 60;
+const timeout_sec = 30 * 60;
+
+var cached_gifs = {
+  first: "",
+  second: "",
+  third: "",
+}
+
+var vid_layers = [ "./first.gif",
+"./second.gif",
+"./third.gif",
+"./fourth.gif" ];
 
 var command = "4th";
 
@@ -40,7 +57,56 @@ function help_info(prefix) {
 function reset() {
   joined = [];
   squad_name = "";
+  rotateIdx = 0;
 }
+
+function generate_gif(index) {
+  return new Promise( (resolve, reject) => {
+     
+
+      output_path = path.join(out_dir, vid_layers[index]);
+      font_loc = path.join(data_dir, "./avengers.ttf");
+      layer_specs = [
+          [{ type: 'title', text: joined[joined.length-1], start: 0, stop: 3.7, zoomDirection: "in", zoomAmount: 0.5, position: "center" }],
+          [{ type: 'title', text: joined[joined.length-1], start: 3, zoomDirection: "in", zoomAmount: 0.5, position: "center" }],
+          [{ type: 'title', text: joined[joined.length-1], start: 3.5, zoomDirection: "in", zoomAmount: 0.5, position: "center" }],
+          [
+              //hardcoded lol but I MEAN ITS CALLED 4th! 4!!!
+              { type: 'title', text: ` ${joined[0]}`, start: 0, stop: 1.2, zoomDirection: "out", zoomAmount: 1, position: {originX:"left", originY: "top", x: 0.1, y: 0.4} },
+              { type: 'title', text: ` ${joined[1]}`, start: 1, stop: 2.2, zoomDirection: "out", zoomAmount: 1, position: {originX:"left", originY: "top", x: 0.01, y: 0.1} },
+              { type: 'title', text: ` ${joined[2]}`, start: 2.5, stop: 3.1, zoomDirection: "out", zoomAmount: 0.1, position: {originX:"left", originY: "top", x: 0.2, y: 0.2} },
+              { type: 'title', text: ` ${joined[3]}`, start: 3, stop: 3.8, zoomDirection: "in", zoomAmount: 0.1, position: {originX:"left", originY: "top", x: 0.1, y: 0.6} },
+              { type: 'title', text: "assemble.", fontPath: font_loc, start: 4.3, zoomDirection: "null", zoomAmount: 0, position: {originX:"left", originY: "top", x: 0.1, y: 0.4} },
+          ],
+      ];
+
+      compiled_layers = [{ type: 'video', path: path.join(data_dir, vid_layers[index])}];
+      compiled_layers = compiled_layers.concat(layer_specs[index]);
+  
+      editSpec = {
+          outPath: output_path,
+          verbose: false,
+          enableFfmpegLog: false,
+          width: 640,
+          height: 360,
+          fps: 10,
+          clips: [
+              { 
+                  layers: compiled_layers
+              }
+          ],
+      };
+  
+      editly(editSpec)
+          .then( () => {
+              resolve(vid_layers[index]);
+          })
+          .catch( e=> {
+              console.log("generate_gif:", e);
+              resolve("Failed to create gif:", e);
+          });
+  });
+}    
 
 function randRange(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -48,6 +114,23 @@ function randRange(min, max) {
 
 function logic() {
   return summons[rotateIdx++ % summons.length];
+}
+
+function avenge_logic() {
+  return new Promise ( (resolve, reject) => {
+    if(rotateIdx < 3 && cached_gifs[rotateIdx-1] == joined[joined.length-1]) {
+      //already there return cached gif
+      output_path = path.join(out_dir, vid_layers[rotateIdx-1]);
+      resolve(output_path);
+    } else {
+      cached_gifs[rotateIdx-1] = joined[joined.length-1];
+      generate_gif(rotateIdx-1)
+      .then( filename => {
+        resolve(host_url + filename);
+      })
+      .catch( (e) => reject("zFailed to create gif:", e));
+    }
+  });
 }
 
 function add_custom_msg(db, username, msg) {
@@ -91,7 +174,6 @@ function execute(args) {
   } else {
     return new Promise((resolve, reject) => {
       db.findOne({ name: args.username }, (err, doc) => {
-        let result = logic();
         let current_time = Date.now() / 1000;
         let res_text = "";
         if (invocated + timeout_sec < current_time) {
@@ -99,32 +181,45 @@ function execute(args) {
           res_text = `<@${args.client}> is trying to summon a 4th!`;
           squad_name = squad_names[randRange(0, squad_names.length)];
           joined.push(`${args.username}`);
+          rotateIdx++;
         } else {
           if (!joined.includes(`${args.username}`)) {
             joined.push(`${args.username}`);
+            rotateIdx++;
             res_text = `<@${args.client}> has joined the call to arms!`;
           } else {
             res_text = `<@${args.client}> still requires heroes! Will you not be a hero?`;
           }
         }
 
-        if (doc) {
-          let msg = doc.message;
-          res_text += `\n\n*"${msg}"*\n\n`;
-        }
+        rotateIdx = Math.min(rotateIdx, total);
 
-        invocated = current_time;
+        avenge_logic()
+        .then( result => {
+          if (doc) {
+            let msg = doc.message;
+            res_text += `\n\n*"${msg}"*\n\n`;
+          }
 
-        if (joined.length >= total) {
-          res_text = `<@${args.client}> **GET HYPED SON!** \nSQUAD ${squad_name} \nis \n**READY!**\n     **PACKED**\n         & **LOADED!**\n\n You can still come on down the *HYPETRAIN*!`;
-          result = `https://media1.tenor.com/images/ffa0709d3d01ee86f926e81acfc0f1e7/tenor.gif`;
-        }
+          invocated = current_time;
 
-        let squad_mems = "";
-        joined.forEach(entry => (squad_mems += `\`${entry}\` `));
-        res_text += ` Type \`/4th\` to join the call to arms!\nSquad  [[ ${squad_name} ]] members:\n ${squad_mems}\n ${result}`;
+          if (joined.length >= total) {
+            res_text = `<@${args.client}> **GET HYPED SON!** \nSQUAD ${squad_name} \nis \n**READY!**\n     **PACKED**\n         & **LOADED!**\n\n You can still come on down the *HYPETRAIN*!`;
+            //result = `https://media1.tenor.com/images/ffa0709d3d01ee86f926e81acfc0f1e7/tenor.gif`;
+          }
 
-        resolve(res_text);
+          let squad_mems = "";
+          joined.forEach(entry => (squad_mems += `\`${entry}\` `));
+          res_text += ` Type \`/4th\` to join the call to arms!\nSquad  [[ ${squad_name} ]] members:\n ${squad_mems}\n`;
+
+          jsonified = {
+            "text": res_text,
+            "attachment":result,
+          }
+          resolve(JSON.stringify(jsonified));
+        })
+        .catch(console.error);
+        
       });
     });
   }
